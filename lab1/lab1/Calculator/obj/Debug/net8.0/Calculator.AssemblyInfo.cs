653a120c71c0602ee127d1307b43cd8f7436d0a3
buryanochka.06@gmail.com <125117368+umunevich@users.noreply.github.com>
2024-11-04 23:13:31 +0200

    Update gitignore

--- lab1/lab1/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ lab1/lab1/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ade053a8d22631b0034ebfe21842b07cd4ca480")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e15091a0f41cc02549ddd140904fe141de21eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- lab1/lab1/CellCraft/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CellCraft.AssemblyInfo.cs
+++ lab1/lab1/CellCraft/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CellCraft.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CellCraft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4ade053a8d22631b0034ebfe21842b07cd4ca480")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e8e15091a0f41cc02549ddd140904fe141de21eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CellCraft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CellCraft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
